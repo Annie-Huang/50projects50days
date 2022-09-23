c587fef15635232bc14e1e1c08b8480326eda6a1
Annie Huang <annie.hn@gmail.com>
2022-09-23 15:57:32 +1000

    Slide in the box from either side once its top is less the  4/5 of the vh.

--- scroll-animation/script.js
+++ scroll-animation/script.js
@@ -2,8 +2,21 @@ const boxes = document.querySelectorAll('.box');
 
 window.addEventListener('scroll', checkBoxes);
 
+// Fire this so it will the first few boxes that is < (window.innerHeight / 5) * 4
 checkBoxes();
 
 function checkBoxes() {
-  console.log(window.innerHeight);
+  // console.log(window.innerHeight);
+
+  const triggerBottom = (window.innerHeight / 5) * 4;
+
+  boxes.forEach((box) => {
+    const boxTop = box.getBoundingClientRect().top;
+
+    if (boxTop < triggerBottom) {
+      box.classList.add('show');
+    } else {
+      box.classList.remove('show');
+    }
+  });
 }
