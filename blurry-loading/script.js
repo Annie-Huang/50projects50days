f82b3ad596244be534017791fb9d66b2062e7b0e
Annie Huang <annie.hn@gmail.com>
2022-09-25 21:29:03 +1000

    !!!! Make the load% appear in the page and also make it slowly fade out when it get to 100%

--- blurry-loading/script.js
+++ blurry-loading/script.js
@@ -12,5 +12,13 @@ function blurring() {
     clearInterval(int);
   }
 
-  console.log(load);
+  // console.log(load);
+
+  loadText.innerText = `${load}%`;
+  loadText.style.opacity = scale(load, 0, 100, 1, 0);
+}
+
+// https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
+function scale(number, inMin, inMax, outMin, outMax) {
+  return ((number - inMin) * (outMax - outMin)) / (inMax - inMin) + outMin;
 }
