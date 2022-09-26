d94ccbcc639ba2d326139836c7d7994084492dc2
Annie Huang <annie.hn@gmail.com>
2022-09-26 21:48:01 +1000

    Make the right-slide go up one image every time we click the UP button.

--- double-vertical-slider/script.js
+++ double-vertical-slider/script.js
@@ -5,7 +5,8 @@ const upButton = document.querySelector('.up-button');
 const downButton = document.querySelector('.down-button');
 const slidesLength = slideRight.querySelectorAll('div').length;
 
-console.log(slidesLength);
+// console.log(slidesLength);
+let activeSlideIndex = 0;
 
 slideLeft.style.top = `-${(slidesLength - 1) * 100}vh`;
 
@@ -14,5 +15,16 @@ upButton.addEventListener('click', () => changeSlide('down'));
 
 const changeSlide = (direction) => {
   const sliderHeight = sliderContainer.clientHeight;
-  console.log(sliderHeight); // when you make the window lower, you will see the value changes.
+  // console.log(sliderHeight); // when you make the window lower, you will see the value changes.
+
+  if (direction === 'up') {
+    activeSlideIndex++;
+    if (activeSlideIndex > slidesLength - 1) {
+      activeSlideIndex = 0;
+    }
+  }
+
+  slideRight.style.transform = `translateY(-${
+    activeSlideIndex * sliderHeight
+  }px)`;
 };
