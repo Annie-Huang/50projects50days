a06cceb6264468e4cfd11850249bd6fa98aa5485
Annie Huang <annie.hn@gmail.com>
2022-09-27 16:53:13 +1000

    Create javascript to split the input text in textarea by ','

---
+++ random-choice-picker/script.js
@@ -0,0 +1,11 @@
+const tagsEl = document.getElementById('tags');
+const textarea = document.getElementById('textarea');
+
+textarea.addEventListener('keyup', (e) => {
+  createTags(e.target.value);
+});
+
+function createTags(input) {
+  const tags = input.split(',');
+  console.log(tags);
+}
