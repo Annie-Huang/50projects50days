ed7312e62abb4f83e7f09a4e5443927e72990e4f
Annie Huang <annie.hn@gmail.com>
2022-09-27 17:08:49 +1000

    Trim and filter out empty tag, and then create each item in a span element and add it into tagsEl

--- random-choice-picker/script.js
+++ random-choice-picker/script.js
@@ -6,6 +6,17 @@ textarea.addEventListener('keyup', (e) => {
 });
 
 function createTags(input) {
-  const tags = input.split(',');
+  const tags = input
+    .split(',')
+    .filter((tag) => tag.trim() !== '')
+    .map((tag) => tag.trim());
   console.log(tags);
+
+  tagsEl.innerHTML = '';
+  tags.forEach((tag) => {
+    const tagEl = document.createElement('span');
+    tagEl.classList.add('tag');
+    tagEl.innerText = tag;
+    tagsEl.appendChild(tagEl);
+  });
 }
