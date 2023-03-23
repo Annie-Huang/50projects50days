7a1bc84700f323f8ada29f46b1fe76e894f73b25
Annie Huang <annie.hn@gmail.com>
2023-03-23 21:36:32 +1100

    Add the progress bar % calculation

--- 03-progress-steps/script.js
+++ 03-progress-steps/script.js
@@ -35,4 +35,9 @@ function update() {
       circle.classList.remove('active');
     }
   });
+
+  const actives = document.querySelectorAll('.active');
+  // console.log(actives.length, circles.length);
+  progress.style.width =
+    ((actives.length - 1) / (circles.length - 1)) * 100 + '%';
 }
