e654b5fb6ef9dc4afc8352a2f45e74c3a2255874
Annie Huang <annie.hn@gmail.com>
2023-04-11 21:25:08 +1000

    Also call the generateJoke() method on button click as well

--- 11-dad-jokes/script.js
+++ 11-dad-jokes/script.js
@@ -1,6 +1,8 @@
 const jokeEl = document.getElementById('joke');
 const jokeBtn = document.getElementById('jokeBtn');
 
+jokeBtn.addEventListener('click', generateJoke);
+
 generateJoke();
 
 function generateJoke() {
@@ -9,6 +11,7 @@ function generateJoke() {
       Accept: 'application/json',
     },
   };
+
   fetch('https://icanhazdadjoke.com', config)
     .then((res) => res.json())
     .then((data) => {
