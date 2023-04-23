b0dda0bf6319f1c17a61074a4b91c712a4e2c7f1
Annie Huang <annie.hn@gmail.com>
2023-04-23 15:57:48 +1000

    Add highlightCups function to full all the cup up to the current clicked one with .full class when I click

--- 17-drink-water/script.js
+++ 17-drink-water/script.js
@@ -2,3 +2,18 @@ const smallCups = document.querySelectorAll('.cup-small');
 const listers = document.getElementById('liters');
 const percentage = document.getElementById('percentage');
 const remained = document.getElementById('remained');
+
+smallCups.forEach((cup, idx) => {
+  cup.addEventListener('click', () => highlightCups(idx));
+});
+
+function highlightCups(idx) {
+  // console.log(idx);
+  smallCups.forEach((cup, idx2) => {
+    if (idx2 <= idx) {
+      cup.classList.add('full');
+    } else {
+      cup.classList.remove('full');
+    }
+  });
+}
