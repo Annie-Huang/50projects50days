c3a76c57c42d9b3f668a1936236f6b8abe01f7e1
Annie Huang <annie.hn@gmail.com>
2023-04-23 16:04:37 +1000

    Add the logic that if the cup I click is the last one that's currently filled, then toggle it.

--- 17-drink-water/script.js
+++ 17-drink-water/script.js
@@ -9,6 +9,17 @@ smallCups.forEach((cup, idx) => {
 
 function highlightCups(idx) {
   // console.log(idx);
+
+  // If the cup I click is the last one that's currently filled, then toggle it.
+  // Add "idx === smallCups.length - 1" (which Brad's code does not have) so it will work for the last cup
+  if (
+    smallCups[idx].classList.contains('full') &&
+    (idx === smallCups.length - 1 ||
+      !smallCups[idx].nextElementSibling.classList.contains('full'))
+  ) {
+    idx--;
+  }
+
   smallCups.forEach((cup, idx2) => {
     if (idx2 <= idx) {
       cup.classList.add('full');
