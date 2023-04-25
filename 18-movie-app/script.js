6a3a6a64a030aba4a4f75f50821a3d7d9dfba128
Annie Huang <annie.hn@gmail.com>
2023-04-25 13:25:35 +1000

    Replace the html content with the info getting from the api. and append movie div one by one into <main>

--- 18-movie-app/script.js
+++ 18-movie-app/script.js
@@ -29,24 +29,32 @@ function showMovies(movies) {
     movieEl.classList.add('movie');
 
     movieEl.innerHTML = `
-      <div class="movie">
         <img
-          src="https://image.tmdb.org/t/p/w1280/qNBAXBIQlnOThrVvA6mA2B5ggV6.jpg"
-          alt=""
+          src="${IMG_PATH + poster_path}"
+          alt="${title}"
         />
         <div class="movie-info">
-          <h3>Movie Title</h3>
-          <span class="green">9.8</span>
+          <h3>${title}</h3>
+          <span class="${getClassByRate(vote_average)}">${vote_average}</span>
         </div>
         <div class="overview">
           <h3>Overview</h3>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur
-          dolorem doloremque ducimus excepturi id magnam odit quaerat qui
-          repudiandae totam.
+          ${overview}
         </div>
-      </div>
       `;
+
+    main.appendChild(movieEl);
   });
+}
+
+function getClassByRate(vote) {
+  if (vote >= 8) {
+    return 'green';
+  } else if (vote >= 5) {
+    return 'orange';
+  } else {
+    return 'red';
+  }
 }
 
 form.addEventListener('submit', (e) => {
