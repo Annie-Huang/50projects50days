3931be8ad28c875b5cd28fc48bd5b8f12af1ca42
Annie Huang <annie.hn@gmail.com>
2023-04-25 13:15:21 +1000

    Create showMovies function

--- 18-movie-app/script.js
+++ 18-movie-app/script.js
@@ -4,6 +4,7 @@ const IMG_PATH = 'https://image.tmdb.org/t/p/w1280';
 const SEARCH_API =
   'https://api.themoviedb.org/3/search/movie?api_key=3fd2be6f0c70a2a598f084ddfb75487c&query="';
 
+const main = document.getElementById('main');
 const form = document.getElementById('form');
 const search = document.getElementById('search');
 
@@ -14,7 +15,38 @@ async function getMovies(url) {
   const res = await fetch(url);
   const data = await res.json();
 
-  console.log(data.results);
+  // console.log(data.results);
+  showMovies(data.results);
+}
+
+function showMovies(movies) {
+  main.innerHTML = '';
+
+  movies.forEach((movie) => {
+    const { title, poster_path, vote_average, overview } = movie;
+
+    const movieEl = document.createElement('div');
+    movieEl.classList.add('movie');
+
+    movieEl.innerHTML = `
+      <div class="movie">
+        <img
+          src="https://image.tmdb.org/t/p/w1280/qNBAXBIQlnOThrVvA6mA2B5ggV6.jpg"
+          alt=""
+        />
+        <div class="movie-info">
+          <h3>Movie Title</h3>
+          <span class="green">9.8</span>
+        </div>
+        <div class="overview">
+          <h3>Overview</h3>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur
+          dolorem doloremque ducimus excepturi id magnam odit quaerat qui
+          repudiandae totam.
+        </div>
+      </div>
+      `;
+  });
 }
 
 form.addEventListener('submit', (e) => {
