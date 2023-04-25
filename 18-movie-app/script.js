f5038badd2eb9237a0370a79870b1ed790877e44
Annie Huang <annie.hn@gmail.com>
2023-04-25 13:07:20 +1000

    Add event listener that on submit for search will trigger getMovies function

--- 18-movie-app/script.js
+++ 18-movie-app/script.js
@@ -4,6 +4,9 @@ const IMG_PATH = 'https://image.tmdb.org/t/p/w1280';
 const SEARCH_API =
   'https://api.themoviedb.org/3/search/movie?api_key=3fd2be6f0c70a2a598f084ddfb75487c&query="';
 
+const form = document.getElementById('form');
+const search = document.getElementById('search');
+
 // Get initial movies
 getMovies(API_URL);
 
@@ -13,3 +16,17 @@ async function getMovies(url) {
 
   console.log(data.results);
 }
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const searchTerm = search.value;
+
+  if (searchTerm && searchTerm !== '') {
+    getMovies(SEARCH_API + searchTerm);
+
+    search.value = '';
+  } else {
+    window.location.reload();
+  }
+});
