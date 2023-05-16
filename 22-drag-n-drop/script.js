97833558fc49509cc68a8842ad36eb0defdb24e1
Annie Huang <annie.hn@gmail.com>
2023-05-16 18:03:04 +1000

    Add dragOver, dragEnter, dragLeave, dragDrop to every .empty div

--- 22-drag-n-drop/script.js
+++ 22-drag-n-drop/script.js
@@ -1,10 +1,17 @@
 const fill = document.querySelector('.fill');
 const empties = document.querySelectorAll('.empty');
 
-// dragstart is logged as soon as you click ont the .fill image
+// dragstart is logged as soon as you click on the .fill image and move
 // dragend is logged as soon as you release the .fill image to another location
 fill.addEventListener('dragstart', dragStart);
 fill.addEventListener('dragend', dragEnd);
+
+for (const empty of empties) {
+  empty.addEventListener('dragover', dragOver);
+  empty.addEventListener('dragenter', dragEnter);
+  empty.addEventListener('dragleave', dragLeave);
+  empty.addEventListener('drop', dragDrop);
+}
 
 function dragStart() {
   console.log('drag start');
@@ -14,6 +21,7 @@ function dragEnd() {
   console.log('drag end');
 }
 
+// Is called whenever you drag and move the image into to another .empty div. the dragOver will continue logging even when you don't move while you are on top of a .empty
 function dragOver() {
   console.log('drag over');
 }
@@ -22,6 +30,7 @@ function dragEnter() {
   console.log('drag enter');
 }
 
+// Is called whenever you leave the image from the current .empty div that you entered
 function dragLeave() {
   console.log('drag leave');
 }
