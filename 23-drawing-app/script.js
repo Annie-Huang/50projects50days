01066bf89185d997f140196dfc99fcaa9f67fa57
Annie Huang <annie.hn@gmail.com>
2023-05-28 15:14:31 +1000

    Setting up the tool box, first identify all the element from Id in the script.js

--- 23-drawing-app/script.js
+++ 23-drawing-app/script.js
@@ -4,6 +4,12 @@
 // https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API/Tutorial/Drawing_shapes#drawing_a_triangle
 
 const canvas = document.getElementById('canvas');
+const increaseBtn = document.getElementById('increase');
+const decreaseBtn = document.getElementById('decrease');
+const sizeEL = document.getElementById('size');
+const colorEL = document.getElementById('color');
+const clearEL = document.getElementById('clear');
+
 const ctx = canvas.getContext('2d');
 
 let size = 20;
