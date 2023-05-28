124888efe93c7710dc5b531a536db017742c09e0
Annie Huang <annie.hn@gmail.com>
2023-05-28 14:24:20 +1000

    Check in code

--- 23-drawing-app/script.js
+++ 23-drawing-app/script.js
@@ -1,6 +1,7 @@
 // https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API
 // https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API/Tutorial/Drawing_shapes
 // https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API/Tutorial/Drawing_shapes#moving_the_pen
+// https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API/Tutorial/Drawing_shapes#drawing_a_triangle
 
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
@@ -17,4 +18,15 @@ function drawCircle(x, y) {
   ctx.fill();
 }
 
+function drawLine(x1, y1, x2, y2) {
+  // https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API/Tutorial/Drawing_shapes#drawing_a_triangle
+  ctx.beginPath();
+  ctx.moveTo(x1, y1);
+  ctx.lineTo(x2, y2);
+  ctx.strokeStyle = color;
+  ctx.lineWidth = size;
+  ctx.stroke();
+}
+
 drawCircle(100, 200);
+drawLine(300, 300, 300, 500);
