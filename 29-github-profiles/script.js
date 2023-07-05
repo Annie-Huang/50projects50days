0443f17af22b51d63893b65580ef50834135e62e
Annie Huang <annie.hn@gmail.com>
2023-07-05 21:55:35 +1000

    Create the root url and the getuser function. Test bradtraversy for username and see can we get info from axios

--- 29-github-profiles/script.js
+++ 29-github-profiles/script.js
@@ -1,1 +1,11 @@
 // https://cdnjs.com/libraries/axios
+
+const APIURL = 'https://api.github.com/users/';
+
+getUser('bradtraversy');
+
+function getUser(username) {
+  axios(APIURL + username)
+    .then((res) => console.log(res))
+    .catch((err) => console.log(err));
+}
