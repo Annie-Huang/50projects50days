1a27b7453184de292f5faca37991f18bb9558127
Annie Huang <annie.hn@gmail.com>
2023-07-12 21:17:09 +1000

    Create our own double click method to control the interval between the 2 consecutive clicks. By marking down the first click time and then the second click calculate the difference and if not working, reset the time

--- 30-double-click-heart/script.js
+++ 30-double-click-heart/script.js
@@ -1,6 +1,20 @@
 const loveMe = document.querySelector('.loveMe');
 const times = document.querySelector('#times');
 
-loveMe.addEventListener('dblclick', (e) => {
-  console.log(123);
+let clickTime = 0;
+// loveMe.addEventListener('dblclick', (e) => {
+//   console.log(123);
+// });
+
+// Smart way to create our own double click function to control the two consecutive clicks interval.
+loveMe.addEventListener('click', (e) => {
+  if (clickTime === 0) {
+    clickTime = new Date().getTime();
+  } else {
+    if (new Date().getTime() - clickTime < 800) {
+      console.log(123);
+    } else {
+      clickTime = new Date().getTime();
+    }
+  }
 });
