fa3436d31c5a4fdef2a8f5df97c9db1dac56f0e6
Annie Huang <annie.hn@gmail.com>
2023-07-16 18:41:21 +1000

    Create the createHeart function to create the heart element first.

--- 30-double-click-heart/script.js
+++ 30-double-click-heart/script.js
@@ -6,15 +6,23 @@ let clickTime = 0;
 //   console.log(123);
 // });
 
-// Smart way to create our own double click function to control the two consecutive clicks interval.
+// Smart way to create our own double click function to control the two consecutive clicks interval time.
 loveMe.addEventListener('click', (e) => {
   if (clickTime === 0) {
     clickTime = new Date().getTime();
   } else {
     if (new Date().getTime() - clickTime < 800) {
-      console.log(123);
+      // console.log(123);
+      createHeart(e);
+      clickTime = 0;
     } else {
       clickTime = new Date().getTime();
     }
   }
 });
+
+const createHeart = (e) => {
+  const heart = document.createElement('i');
+  heart.classList.add('fas');
+  heart.classList.add('fa-heart');
+};
