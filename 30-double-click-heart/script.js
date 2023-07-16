25eeace970ccbed8adc888eef2739fded5d30c9a
Annie Huang <annie.hn@gmail.com>
2023-07-16 18:58:10 +1000

    Calculate the correct asolute position of the click in relation to the image and append the heart element into loveMe

--- 30-double-click-heart/script.js
+++ 30-double-click-heart/script.js
@@ -25,4 +25,20 @@ const createHeart = (e) => {
   const heart = document.createElement('i');
   heart.classList.add('fas');
   heart.classList.add('fa-heart');
+
+  const x = e.clientX;
+  const y = e.clientY;
+
+  // The offset point of the top left corner of the image
+  const leftOffset = e.target.offsetLeft;
+  const topOffset = e.target.offsetTop;
+
+  const xInside = x - leftOffset;
+  const yInside = y - topOffset;
+
+  // In the css, the '.loveMe .fa-heart' is already set to position absolute.
+  heart.style.top = `${yInside}px`;
+  heart.style.left = `${xInside}px`;
+
+  loveMe.appendChild(heart);
 };
