629b73d12ba44d1edfebc83e1c1380356d1202b4
Annie Huang <annie.hn@gmail.com>
2023-07-22 13:17:20 +1000

    Add writeText function to have a timeout and write letter one by one. When it gets to the end, reset it from the beginning.

--- 31-auto-text-effect/script.js
+++ 31-auto-text-effect/script.js
@@ -3,3 +3,16 @@ const speedEL = document.getElementById('speed');
 const text = 'We Love Programming!';
 let idx = 1;
 let speed = 300 / speedEL.value;
+
+writeText();
+
+function writeText() {
+  textEL.innerText = text.slice(0, idx);
+  idx++;
+
+  if (idx > text.length) {
+    idx = 1;
+  }
+
+  setTimeout(writeText, speed);
+}
