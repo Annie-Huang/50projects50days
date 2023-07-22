ee3585a9300b54851d0daecaaa30dc5913737f2a
Annie Huang <annie.hn@gmail.com>
2023-07-22 13:07:06 +1000

    Get all the elements in script.js and define variables

---
+++ 31-auto-text-effect/script.js
@@ -0,0 +1,5 @@
+const textEL = document.getElementById('text');
+const speedEL = document.getElementById('speed');
+const text = 'We Love Programming!';
+let idx = 1;
+let speed = 300 / speedEL.value;
