8ed784f69852ffe24da16bc7e96ff20b47d59629
Annie Huang <annie.hn@gmail.com>
2023-08-04 15:29:38 +1000

    Create runAnimation function and for reach number, if animationend got the animation keyframe of 'goIn', swap .in to .out class

--- 35-animated-countdown/script.js
+++ 35-animated-countdown/script.js
@@ -2,3 +2,20 @@ const nums = document.querySelectorAll('.nums span');
 const counter = document.querySelector('.counter');
 const finalMessage = document.querySelector('.final');
 const replay = document.querySelector('#replay');
+
+runAnimation();
+
+function runAnimation() {
+  const nextToLast = nums.length - 1;
+
+  nums.forEach((num, idx) => {
+    console.log(num, idx);
+
+    num.addEventListener('animationend', (e) => {
+      if (e.animationName === 'goIn' && idx !== nextToLast) {
+        num.classList.remove('in');
+        num.classList.add('out');
+      }
+    });
+  });
+}
