535ed7082059b4c37f40805d3b38d5778556fc28
Annie Huang <annie.hn@gmail.com>
2023-08-07 14:21:16 +1000

    Change it back into 150 cards

--- 38-pokedex/script.js
+++ 38-pokedex/script.js
@@ -1,7 +1,7 @@
 // https://pokeapi.co/
 
 const poke_container = document.getElementById('poke-container');
-const pokemon_count = 5;
+const pokemon_count = 150;
 const colors = {
   fire: '#FDDFDF',
   grass: '#DEFDE0',
