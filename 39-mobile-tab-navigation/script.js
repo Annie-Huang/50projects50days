20e433c0304eea5cfbdfa80308ee112d8bce9577
Annie Huang <annie.hn@gmail.com>
2023-08-15 17:57:53 +1000

    Create event listener on each menu item click, first to hide and content (images) and hide the items (menu items).

---
+++ 39-mobile-tab-navigation/script.js
@@ -0,0 +1,9 @@
+const contents = document.querySelectorAll('.content');
+const listItems = document.querySelectorAll('nav ul li');
+
+listItems.forEach((item, idx) => {
+  item.addEventListener('click', () => {
+    hideAllContent();
+    hideAllItems();
+  });
+});
