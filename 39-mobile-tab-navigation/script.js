eb5b82be363bc8611b2038ab1e8d81ae0034024c
Annie Huang <annie.hn@gmail.com>
2023-08-15 17:59:50 +1000

    Implement hideAllContents and hideAllItems function

--- 39-mobile-tab-navigation/script.js
+++ 39-mobile-tab-navigation/script.js
@@ -3,7 +3,14 @@ const listItems = document.querySelectorAll('nav ul li');
 
 listItems.forEach((item, idx) => {
   item.addEventListener('click', () => {
-    hideAllContent();
+    hideAllContents();
     hideAllItems();
   });
 });
+
+function hideAllContents() {
+  contents.forEach((content) => content.classList.remove('show'));
+}
+function hideAllItems() {
+  listItems.forEach((item) => item.classList.remove('active'));
+}
