4d69dd8f60a0bdc2c4237e17111929947ca2ea95
Annie Huang <annie.hn@gmail.com>
2023-08-16 12:26:26 +1000

    Reduce blurness when the password length is increasing

--- 40-password-strength-background/script.js
+++ 40-password-strength-background/script.js
@@ -3,5 +3,10 @@ const background = document.getElementById('background');
 
 password.addEventListener('input', (e) => {
   const input = e.target.value;
-  console.log(input);
+  // console.log(input);
+
+  const length = input.length;
+  // console.log(length);
+  const blurValue = 20 - length * 2;
+  background.style.filter = `blur(${blurValue}px)`;
 });
