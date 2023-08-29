d563bc35c1b800fc729551141edd040be92474d5
Annie Huang <annie.hn@gmail.com>
2023-08-29 18:01:40 +1000

    Add event listener to every input key, if user type a 0-9 digits, move focus to the next <input>

--- 42-verify-account-ui/script.js
+++ 42-verify-account-ui/script.js
@@ -2,3 +2,11 @@ const codes = document.querySelectorAll('.code');
 
 // On landing, focus on the first <input>
 codes[0].focus();
+
+codes.forEach((code, idx) => {
+  code.addEventListener('keydown', (e) => {
+    if (e.key >= 0 && e.key <= 9) {
+      codes[idx + 1].focus();
+    }
+  });
+});
