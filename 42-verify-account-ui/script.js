b2f2d5a113d7d8062b42a793504ce8d0bc88f177
Annie Huang <annie.hn@gmail.com>
2023-08-30 15:06:36 +1000

    Force it to wait to finish writing, before shifting focus.

--- 42-verify-account-ui/script.js
+++ 42-verify-account-ui/script.js
@@ -6,7 +6,10 @@ codes[0].focus();
 codes.forEach((code, idx) => {
   code.addEventListener('keydown', (e) => {
     if (e.key >= 0 && e.key <= 9) {
-      codes[idx + 1].focus();
+      // codes[idx + 1].focus();
+      // Force it to wait to finish writing, before shifting focus.
+      // Otherwise it will not be possible to write into the first <input>, not starting from the second one.
+      setTimeout(() => codes[idx + 1].focus(), 10);
     }
   });
 });
