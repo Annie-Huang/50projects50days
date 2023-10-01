2fc115d078b3716b85b8d0071f514e3f130f8f4c
Annie Huang <annie.hn@gmail.com>
2023-10-01 19:04:29 +1100

    When the <img> and <small> is clicked, switch active class into the current card.

--- 44-feedback-ui-design/script.js
+++ 44-feedback-ui-design/script.js
@@ -2,6 +2,19 @@ const ratings = document.querySelectorAll('.rating');
 const sendBtn = document.querySelector('#send');
 const panel = document.querySelector('#panel');
 
-panel.addEventListener('click', () => {
-  console.log('click');
+panel.addEventListener('click', (e) => {
+  // console.log(e.target);
+
+  // When the <img> or <small> is clicked:
+  if (e.target.parentNode.classList.contains('rating')) {
+    console.log(e.target);
+    removeActive();
+    e.target.parentNode.classList.add('active');
+  }
 });
+
+function removeActive() {
+  for (let i = 0; i < ratings.length; i++) {
+    ratings[i].classList.remove('active');
+  }
+}
