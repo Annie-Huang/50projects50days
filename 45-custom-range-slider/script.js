bf618d765d5eab87c0cef0d37d26274433a387e2
Annie Huang <annie.hn@gmail.com>
2023-11-15 16:59:54 +1100

    Copy and scale function in from map a range of numbers to another rnage of numbers

--- 45-custom-range-slider/script.js
+++ 45-custom-range-slider/script.js
@@ -21,3 +21,8 @@ range.addEventListener('input', (e) => {
 
   label.innerHTML = value;
 });
+
+// https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
+function scale(number, inMin, inMax, outMin, outMax) {
+  return ((number - inMin) * (outMax - outMin)) / (inMax - inMin) + outMin;
+}
