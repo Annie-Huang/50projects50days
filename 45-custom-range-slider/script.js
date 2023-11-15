69956d4aa9b102e308a140d5ba1f1a538a868fd5
Annie Huang <annie.hn@gmail.com>
2023-11-15 16:49:20 +1100

    Get the value without 'px'

--- 45-custom-range-slider/script.js
+++ 45-custom-range-slider/script.js
@@ -7,7 +7,9 @@ range.addEventListener('input', (e) => {
   const range_width = getComputedStyle(e.target).getPropertyValue('width');
   const label_width = getComputedStyle(label).getPropertyValue('width');
 
-  console.log(range_width, label_width);
+  const num_width = +range_width.slice(0, -2);
+  const num_label_width = +label_width.slice(0, -2);
+  console.log(num_width, num_label_width);
 
   label.innerHTML = value;
 });
