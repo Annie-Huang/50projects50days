f696b24dbc4df496e84e041dddacc129f07eba36
Annie Huang <annie.hn@gmail.com>
2023-12-26 10:49:55 +1100

    Implement the choose_insect_btn click function

--- 51-catch-the-insect/script.js
+++ 51-catch-the-insect/script.js
@@ -14,3 +14,13 @@ let selected_insect = {};
 
 // Click to go to 2nd screen
 start_btn.addEventListener('click', () => screens[0].classList.add('up'));
+
+choose_insect_btns.forEach((btn) => {
+  btn.addEventListener('click', () => {
+    const img = btn.querySelector('img');
+    const src = img.getAttribute('src');
+    const alt = img.getAttribute('alt');
+    selected_insect = { src, alt };
+    screens[1].classList.add('up');
+  });
+});
