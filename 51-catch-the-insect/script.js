0f3b3a9c923b3b722540abbdc485fe931cabafa0
Annie Huang <annie.hn@gmail.com>
2023-12-26 11:03:49 +1100

    Add insect into DOM and create catchInsect function. Test to see if we can get into the function.

--- 51-catch-the-insect/script.js
+++ 51-catch-the-insect/script.js
@@ -39,6 +39,9 @@ function createInsect() {
     src="${selected_insect.src}"
     alt="${selected_insect.alt}" 
     style="transform: rotate(${Math.random() * 360}deg)" />`;
+
+  insect.addEventListener('click', catchInsect);
+  game_container.appendChild(insect);
 }
 
 function getRandomLocation() {
@@ -47,4 +50,8 @@ function getRandomLocation() {
   const x = Math.random() * (width - 200) + 100;
   const y = Math.random() * (height - 200) + 100;
   return { x, y };
+}
+
+function catchInsect() {
+  console.log('123');
 }
